e610a920bcdcc62e4605bcf5c1c748ffdb81fe28
Richard Halford <richard@xhalford.com>
2022-03-12 16:07:19 +0000

    feature(nvim): neovim package management
    Implemented a package manager via nix utilities. Setting up filepath
    discovery for lua config files for vim. Including language server
    support and omnifunc completion support.

--- modules/users/development/editor.nix
+++ modules/users/development/editor.nix
@@ -1,8 +1,18 @@
-{ pkgs, config, lib, ...}:
+{ pkgs, config, lib, ... }:
 with lib;
 
 let
   cfg = config.richard.development.editor;
+
+  pluginGit = ref: repo: pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "${lib.strings.sanitizeDerivationName repo}";
+    version = ref;
+    src = builtins.fetchGit {
+      url = "https://github.com/${repo}.git";
+      ref = ref;
+    };
+  };
+  plugin = pluginGit "HEAD";
 in
 {
   options.richard.development.editor = {
@@ -18,18 +28,44 @@ in
       neovim = {
         enable = true;
         package = pkgs.neovim-nightly;
-	# extraConfig = "";
-	# extraLuaPackages = "[]";
-	# extraPackages = with pkgs; = [ ];
-	# extraPython3Packages = "ps: []";
-	# generatedConfigViml = "";
-	# plugins = [ ];
-	# viAlias = true;
-	# vimAlias = true;
-	# vimdiffAlias = true;
-	# withNodeJs = true;
-	# with Python3 = true;
-	# withRuby = true;
+        extraConfig = builtins.concatStringsSep "\n" [
+          ''
+            luafile ${builtins.toString ./nvim/init_lua.lua}
+            luafile ${builtins.toString ./nvim/lua/keymaps.lua}
+            luafile ${builtins.toString ./nvim/lua/options.lua}
+            luafile ${builtins.toString ./nvim/lua/config/cmp.lua}
+            luafile ${builtins.toString ./nvim/lua/config/lsp.lua}
+          ''
+        ];
+        extraPackages = with pkgs; [
+          gopls
+          nodePackages.bash-language-server
+          nodePackages.pyright
+          nodePackages.svelte-language-server
+          nodePackages.vim-language-server
+          nodePackages.vscode-langservers-extracted
+          nodePackages.vue-language-server
+          nodePackages.yaml-language-server
+          rnix-lsp
+        ];
+        plugins = with pkgs.vimPlugins; [
+          (plugin "gruvbox-community/gruvbox")
+          (plugin "neovim/nvim-lspconfig")
+          (plugin "hrsh7th/nvim-cmp")
+          (plugin "L3MON4D3/LuaSnip")
+          (plugin "hrsh7th/cmp-buffer")
+          (plugin "hrsh7th/cmp-nvim-lsp")
+          (plugin "hrsh7th/cmp-emoji")
+          (plugin "hrsh7th/cmp-nvim-lua")
+          (plugin "hrsh7th/cmp-path")
+          (plugin "saadparwaiz1/cmp_luasnip")
+        ];
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+        withNodeJs = true;
+        withPython3 = true;
+        withRuby = true;
       };
     };
   };
---
+++ modules/users/development/nvim/init_lua.lua
@@ -0,0 +1,50 @@
+local g = vim.g
+local cmd = vim.cmd
+
+-- require 'filetypes'
+-- require 'keymaps'
+-- require 'options'
+-- require 'plugins'
+
+-- TODO: find a home for the below commands and convert to lua.
+-- Python Venv
+-- g.python3_host_prog = '/usr/bin/python3'
+
+--  Highlight on Yank
+cmd [[ autocmd TextYankPost * silent! lua vim.highlight.on_yank() ]]
+
+-- Copy & Paste
+cmd [[
+let &t_SI .= "\<Esc>[?2004h"
+let &t_EI .= "\<Esc>[?2004l"
+inoremap <special> <expr> <Esc>[200~ XTermPasteBegin()
+function! XTermPasteBegin()
+	set pastetoggle=<Esc>[201~
+	set paste
+	return ""
+endfunction
+]]
+
+-- Gruvbox
+g.gruvbox_contrast_dark = 'hard'
+cmd [[
+if exists('+termguicolors')
+  let &t_8f = "\<Esc>[38;2;%lu;%lu;%lum"
+  let &t_8b = "\<Esc>[48;2;%lu;%lu;%lum"
+endif
+]]
+g.gruvbox_invert_selection = '0'
+cmd [[
+colorscheme gruvbox
+highlight ColorColumn guibg=#282828
+highlight CursorColumn guibg=#282828
+highlight CursorLine guibg=#282828
+highlight CursorLineNR guibg=#282828
+highlight GitSignsAddNr guibg=None guifg=#b8bb26
+highlight GitSignsChangeNr guibg=None guifg=#8ec07c
+highlight GitSignsDeleteNr guibg=None guifg=#fb4934
+highlight EndOfBuffer guifg=#1d2021
+highlight VertSplit guifg=#282828
+highlight VertSplit guibg=#282828
+highlight Comment gui=italic
+]]
