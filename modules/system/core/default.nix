9ac99bde35904fb4c0cc48edd829e255ccfd3d0c
Richard Halford <richard@xhalford.com>
2022-03-17 13:50:35 +0000

    feature(cachix): implemented cachix for nix-community
    Prevent compiling popular packages supported by the nix-community.

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -40,6 +40,7 @@ in
       pathsToLink = [ "/share/zsh" ];
       shells = [ pkgs.zsh ];
       systemPackages = with pkgs; [
+        cachix
         git
         libnotify
         neovim
@@ -67,6 +68,10 @@ in
           # keep-derivations = true;
           experimental-features = nix-command flakes
         '';
+      settings = {
+        substituters = [ "https://nix-community.cachix.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+      };
     };
 
     programs.sway.enable = true;
