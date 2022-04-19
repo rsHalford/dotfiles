b82092b4cc9b5881b0a0805da2cccba18d8d790a
Richard Halford <dev@rshalford.com>
2022-04-19 19:56:29 +0100

    feat(scripts): sys cli utility for managing system

--- scripts/default.nix
+++ scripts/default.nix
@@ -8,6 +8,77 @@ let
     notify-send "Grim" "Screenshot Taken\n$(wl-paste)"
   '';
 
+  sysTools = with pkgs; writeScriptBin "sys" ''
+    #!${runtimeShell}
+    case $1 in
+        "apply")
+            cd ~/.dotfiles || exit
+            if [ -z "$2" ]; then
+                sudo nixos-rebuild switch --flake '.#'
+            elif [ "$2" = "--user" ] || [ "$2" = "-u" ] ; then
+                nix build --impure .#homeManagerConfigurations."''${USER}".activationPackage
+                ./result/activate
+            elif [ "$2" = "--boot" ] || [ "$2" = "-b" ] ; then
+                sudo nixos-rebuild boot --flake '.#'
+            elif [ "$2" = "--test" ] || [ "$2" = "-c" ] ; then
+                sudo nixos-rebuild test --flake '.#'
+            elif [ "$2" = "--check" ] || [ "$2" = "-t" ] ; then
+                nixos-rebuild dry-activate --flake '.#'
+            else
+                echo "Unknown option: $2"
+            fi
+            cd - || exit
+        ;;
+
+        "clean")
+            printf "%s\n" "Performing garbage collection on store"
+            nix store gc
+            printf "%s\n" "Replacing identical files in the store by hard links"
+            nix store optimise
+        ;;
+
+        "depends")
+            nix-store -qR "$(which "$2")"
+        ;;
+
+        "pkgs")
+            nix-store -qR /run/current-system | sed -n -e 's/\/nix\/store\/[0-9a-z]\{32\}-//p' | sort | uniq
+        ;;
+
+        "update")
+            cd ~/.dotfiles || exit
+            nix flake update
+            # --commit-lock-file --commit-lockfile-summary "Custom Commit Message"
+            cd - || exit
+        ;;
+
+        "which")
+            nix show-derivation "$(which "$2")" | jq -r '.[].outputs.out.path'
+        ;;
+
+        "help"|*)
+            COMMANDS="apply
+    clean
+    depends
+    pkgs
+    update
+    which"
+
+            DESCRIPTIONS="Applies current system configuration defined in dotfiles.
+    Runs garbage collection and creates hard-links within nix store.
+    Lists dependencies for package.
+    Lists all installed packages in nix store.
+    Updates the dotfiles flake.lock.
+    Returns the path of the given command."
+
+            printf "%s\n" "Usage: sys [COMMAND] [OPTIONS]"
+            printf "%s\n\n" "Utility tool to manage your NixOS system"
+            printf "%s\n" "Commands:"
+            paste <(printf %s "$COMMANDS") <(printf %s "$DESCRIPTIONS")
+        ;;
+    esac
+  '';
+
   worktreeTools = with pkgs; writeScriptBin "worktree-clone" ''
     #!${runtimeShell}
     URL=$1
@@ -25,6 +96,7 @@ in
 {
   overlay = (final: prev: {
     scripts.screenshotTools = screenshotTools;
+    scripts.sysTools = sysTools;
     scripts.worktreeTools = worktreeTools;
   });
 }
