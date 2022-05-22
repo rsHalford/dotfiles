968d469e069ff40f29b79ea4e7901e36653bbb5d
Richard Halford <dev@rshalford.com>
2022-05-22 12:39:03 +0100

    fix(media): reconfigure bindings for ncmpcpp

--- modules/users/media/default.nix
+++ modules/users/media/default.nix
@@ -54,11 +54,10 @@ in
           { key = "shift-up"; command = [ "select_item" "scroll_up" ]; }
           { key = "ctrl-u"; command = "page_up"; }
           { key = "ctrl-d"; command = "page_down"; }
-          { key = "u"; command = "page_up"; }
-          { key = "d"; command = "page_down"; }
           { key = "h"; command = "previous_column"; }
           { key = "l"; command = "next_column"; }
-          { key = "."; command = "show_lyrics"; }
+          { key = "L"; command = "show_lyrics"; }
+          { key = "alt-f"; command = "toggle_lyrics_fetcher"; }
           { key = "n"; command = "next_found_item"; }
           { key = "N"; command = "previous_found_item"; }
           { key = "J"; command = "move_sort_order_down"; }
@@ -67,19 +66,17 @@ in
           { key = "l"; command = "enter_directory"; }
           { key = "l"; command = "run_action"; }
           { key = "l"; command = "play_item"; }
-          { key = "m"; command = "show_media_library"; }
           { key = "m"; command = "toggle_media_library_columns_mode"; }
-          { key = "t"; command = "show_tag_editor"; }
-          { key = "v"; command = "show_visualizer"; }
           { key = "g"; command = "move_home"; }
           { key = "G"; command = "move_end"; }
-          { key = "U"; command = "update_database"; }
-          { key = "s"; command = "reset_search_engine"; }
-          { key = "s"; command = "show_search_engine"; }
-          { key = "f"; command = "show_browser"; }
-          { key = "f"; command = "change_browse_mode"; }
-          { key = "x"; command = "delete_playlist_items"; }
-          { key = "P"; command = "show_playlist"; }
+          { key = "b"; command = "change_browse_mode"; }
+          { key = "d"; command = "delete_playlist_items"; }
+          { key = "."; command = "seek_forward"; }
+          { key = ","; command = "seek_backward"; }
+          { key = "ctrl-v"; command = "select_range"; }
+          { key = "V"; command = "remove_selection"; }
+          { key = "ctrl-k"; command = "move_selected_items_up"; }
+          { key = "ctrl-j"; command = "move_selected_items_down"; }
         ];
         mpdMusicDir = null;
         settings = {
