790b39906f35a5caac891b68229a1ad9c05429b2
Richard Halford <dev@rshalford.com>
2022-06-18 14:38:18 +0100

    feat(direnv): added layout support for projects using postgres

--- modules/users/direnv/default.nix
+++ modules/users/direnv/default.nix
@@ -23,16 +23,34 @@ in
       enable = true;
       enableZshIntegration = true;
       nix-direnv.enable = true;
-      stdlib =
-        ''
-          declare -A direnv_layout_dirs
-          direnv_layout_dir() {
+      stdlib = ''
+        declare -A direnv_layout_dirs
+        direnv_layout_dir() {
             echo "''${direnv_layout_dirs[$PWD]:=$(
                 echo -n ${config.xdg.cacheHome}/direnv/layouts/
                 echo -n "$PWD" | shasum | cut -d ' ' -f 1
-                )}"
-          }
-        '';
+                                               )}"
+        }
+
+        layout_postgres() {
+            export PGDIR="$(direnv_layout_dir)/postgres"
+            export PGHOST="$PGDIR"
+            export PGDATA="$PGDIR/data"
+            export PGLOG="$PGDIR/log"
+            export PGCONF="$PGDATA/postgresql.conf"
+
+            if [ ! -d "$PGDIR" ]; then
+                mkdir -p "$PGDIR"
+            fi
+
+            if [ ! -d "$PGDATA" ]; then
+                printf "%s\n" "Initialising postgresql database..."
+                initdb
+                printf "%s\n%s'%s'\n" "listen_addresses = '''" "unix_socket_directories = " $PGHOST >> $PGCONF
+                printf '%s%s%s\n' 'CREATE DATABASE ' $USER ';' | postgres --single -E postgres
+            fi
+        }
+      '';
     };
   };
 }
