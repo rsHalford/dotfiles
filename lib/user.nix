cdefc2fe2eeecabd91c00ac7b92bc2452905bac2
Richard Halford <dev@rshalford.com>
2022-06-26 13:24:37 +0100

    fix: homeManagerConfiguration changes for 22.11

--- lib/user.nix
+++ lib/user.nix
@@ -3,37 +3,34 @@ with builtins;
 {
   mkHMUser = { userConfig, username }:
     home-manager.lib.homeManagerConfiguration {
-      inherit system username pkgs;
-      stateVersion = "21.11";
-      configuration =
+      inherit pkgs;
+      modules =
         let
           trySettings = tryEval (fromJSON (readFile /etc/hmsystemdata.json));
           machineData = if trySettings.success then trySettings.value else { };
-
           machineModule = { pkgs, config, lib, ... }: {
             options.machineData = lib.mkOption {
               default = { };
               description = "Settings passed from nixos system configuration. If not present will be empty";
             };
-
             config.machineData = machineData;
           };
         in
-        {
-          richard = userConfig;
-
-          nixpkgs.overlays = overlays;
-          nixpkgs.config.allowUnfree = true;
-
-          systemd.user.startServices = true;
-          home = {
-            stateVersion = "21.11";
-            username = username;
-            homeDirectory = "/home/${username}";
-          };
-          imports = [ ../modules/users machineModule ];
-        };
-      homeDirectory = "/home/${username}";
+        [
+          {
+            richard = userConfig;
+            nixpkgs.overlays = overlays;
+            nixpkgs.config.allowUnfree = true;
+            systemd.user.startServices = true;
+            home = {
+              stateVersion = "21.11";
+              username = username;
+              homeDirectory = "/home/${username}";
+            };
+          }
+          ../modules/users
+          machineModule
+        ];
     };
 
   mkSystemUser = { description, groups, name, shell, uid, ... }:
