9ba76761aa1d9125596813c1362bef070c713f9d
Richard Halford <richard@xhalford.com>
2022-07-09 10:26:01 +0100

    feat(laptop): improve system power settings
    Hopefully reduce overheating when on AC, and just act like when on battery.

--- modules/system/laptop/default.nix
+++ modules/system/laptop/default.nix
@@ -24,14 +24,14 @@ in
   config = mkIf (cfg.enable) (mkMerge [
     ({
       environment.systemPackages = with pkgs; [
-        # acpid
-        # powertop
+        acpid
+        powertop
         brightnessctl
       ];
 
-      # powerManagement = {
-      #   cpuFreqGovernor = "powersave";
-      # };
+      powerManagement = {
+        cpuFreqGovernor = "powersave";
+      };
 
       # systemd = {
       #   sleep.extraConfig =
@@ -65,7 +65,7 @@ in
         tlp = {
           enable = true;
           settings = {
-            "SOUND_POWER_SAVE_ON_AC" = 0;
+            "SOUND_POWER_SAVE_ON_AC" = 1;
             "SOUND_POWER_SAVE_ON_BAT" = 1;
             "SOUND_POWER_SAVE_CONTROLLER" = "Y";
             "START_CHARGE_THRESH_BAT0" = 45;
@@ -75,7 +75,7 @@ in
             "DISK_APM_LEVEL_ON_AC" = "254 254";
             "DISK_APM_LEVEL_ON_BAT" = "128 128";
             "DISK_IOSCHED" = "mq-deadline mq-deadline";
-            "SATA_LINKPWR_ON_AC" = "med_power_with_dipm max_performance";
+            "SATA_LINKPWR_ON_AC" = "med_power_with_dipm min_performance";
             "SATA_LINKPWR_ON_BAT" = "min_power min_power";
             "MAX_LOST_WORK_SECS_ON_AC" = 15;
             "MAX_LOST_WORK_SECS_ON_BAT" = 60;
@@ -86,17 +86,17 @@ in
             "CPU_SCALING_GOVERNOR_ON_AC" = "powersave";
             "CPU_SCALING_GOVERNOR_ON_BAT" = "powersave";
             "CPU_MIN_PERF_ON_AC" = 0;
-            "CPU_MAX_PERF_ON_AC" = 100;
+            "CPU_MAX_PERF_ON_AC" = 50;
             "CPU_MIN_PERF_ON_BAT" = 0;
             "CPU_MAX_PERF_ON_BAT" = 50;
             "CPU_BOOST_ON_AC" = 1;
             "CPU_BOOST_ON_BAT" = 1;
             "SCHED_POWERSAVE_ON_AC" = 0;
             "SCHED_POWERSAVE_ON_BAT" = 1;
-            "CPU_ENERGY_PERF_POLICY_ON_AC" = "performance";
+            "CPU_ENERGY_PERF_POLICY_ON_AC" = "power";
             "CPU_ENERGY_PERF_POLICY_ON_BAT" = "power";
             "RESTORE_DEVICE_STATE_ON_STARTUP" = 0;
-            "RUNTIME_PM_ON_AC" = "on";
+            "RUNTIME_PM_ON_AC" = "auto";
             "RUNTIME_PM_ON_BAT" = "auto";
             "PCIE_ASPM_ON_AC" = "default";
             "PCIE_ASPM_ON_BAT" = "powersupersave";
