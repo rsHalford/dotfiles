efc7f4a87cf8c3fae9b73f6248689dad4c6eb3fe
Richard Halford <dev@rshalford.com>
2022-07-26 18:46:04 +0100

    feat(core) set firefox as default browser
    Need to modularise for current $BROWSER

--- modules/users/core/default.nix
+++ modules/users/core/default.nix
@@ -73,9 +73,9 @@ in
           "x-scheme-handler/mailto" = "thunderbird.desktop";
           # "text/plain" = "emacsclient.desktop";
           # "text/x-shellscript" = "emacsclient.desktop";
-          "text/html" = "org.qutebrowser.qutebrowser.desktop";
-          "x-scheme-handler/http" = "org.qutebrowser.qutebrowser.desktop";
-          "x-scheme-handler/https" = "org.qutebrowser.qutebrowser.desktop";
+          "text/html" = "firefox.desktop";
+          "x-scheme-handler/http" = "firefox.desktop";
+          "x-scheme-handler/https" = "firefox.desktop";
           "application/pdf" = "org.pwmt.zathura.desktop";
           "application/postscript" = "org.pwmt.zathura.desktop";
           "x-scheme-handler/magnet" = "org.qbittorrent.qBittorrent.desktop";
