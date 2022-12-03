ab27883bb26efef0aa6756bfed9b714ac6ea7dbb
Richard Halford <dev@rshalford.com>
2022-12-03 12:12:42 +0000

    fix(kanshi): match asymmetrical monitor scaling

--- modules/users/services/default.nix
+++ modules/users/services/default.nix
@@ -102,7 +102,7 @@ in
               {
                 criteria = "BenQ Corporation BenQ EX2510 9BL02077019";
                 mode = "1920x1080@144Hz";
-                position = "0,370";
+                position = "0,275";
                 scale = 1.0;
                 status = "enable";
               }
