6720a8a2f7fdb34742f63770a536c47714b9f2c6
Richard Halford <dev@rshalford.com>
2022-12-11 18:09:31 +0000

    style(tty): rose-pine tty theme

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -15,7 +15,24 @@ in
 
   config = mkIf (cfg.enable) {
     console = {
-      # colors = [ ];
+      colors = [
+        "232136"
+        "eb6f92"
+        "9ccfd8"
+        "f6c177"
+        "3e8fb0"
+        "c4a7e7"
+        "ea9a97"
+        "e0def4"
+        "393552"
+        "eb6f92"
+        "9ccfd8"
+        "f6c177"
+        "3e8fb0"
+        "c4a7e7"
+        "ea9a97"
+        "e0def4"
+      ];
       # font = "";
       keyMap = "uk";
       # packages = with pkgs; [ ];
