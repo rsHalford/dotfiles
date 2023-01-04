57d1987f0062b344045eb6fa0d6c7b1ae79bd099
Richard Halford <dev@rshalford.com>
2023-01-04 22:49:03 +0000

    feat(nvim): improved zen-mode writing setup

--- modules/users/development/nvim/lua/config/prose.lua
+++ modules/users/development/nvim/lua/config/prose.lua
@@ -47,7 +47,7 @@ twilight.setup {
 -- markdown preview
 local g = vim.g
 
-g.mkdp_auto_start = 1
+g.mkdp_auto_start = 0
 g.mkdp_auto_close = 0
 g.mkdp_refresh_slow = 0
 g.mkdp_command_for_global = 0
@@ -59,24 +59,16 @@ vim.api.nvim_create_autocmd('VimEnter', {
   group = proseG,
   pattern = { '*.ms', '*.mom', '*.me', '.man', '*.md', '*.tex', '*.org', '*.norg', '*.txt' },
   callback = function()
-    require('zen-mode').toggle()
+    require('zen-mode').toggle({
+      plugins = {
+        options = {
+          wrap = true,
+          linebreak = true,
+        },
+      },
+    })
     vim.opt.spell = true
   end, desc = 'Enter Prose writing mode'
 })
 
 vim.keymap.set('n', '<leader>tp', [[<cmd>lua require('zen-mode').toggle()<CR>]], { desc = 'Toggle Zen Mode' })
-
--- g.pencil#textwidth = 70
--- vim.cmd [[
--- augroup prose
---   autocmd!
---   autocmd FileType groff,mail,markdown,text ":ZenMode
---     \ | execute 'DittoOn'
---     \ | set spell
---     \ | call pencil#init({'wrap': 'soft'})
---     \ | call litecorrect#init()
---     \ | call textobj#quote#init()
---     \ | call textobj#sentence#init()
---     \ | call wordy#init()
--- augroup END
--- ]]
