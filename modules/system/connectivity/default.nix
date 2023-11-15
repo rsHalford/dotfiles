11f296260af76ec5f5ddf0a0251c27a371e3f471
Richard Halford <dev@rshalford.com>
2023-11-15 12:51:41 +0000

    feat(vm): configuration to support Windows 11 on virt-manager

--- modules/system/connectivity/default.nix
+++ modules/system/connectivity/default.nix
@@ -74,6 +74,27 @@ in {
       package = pkgs.mullvad-vpn;
     };
 
-    virtualisation.libvirtd.enable = cfg.virtualisation.enable;
+    environment.systemPackages = with pkgs; mkIf (cfg.virtualisation.enable) [
+      virt-manager
+      virt-viewer
+      spice
+      spice-gtk
+      win-virtio
+      win-spice
+      gnome.adwaita-icon-theme
+    ];
+
+    virtualisation = {
+      libvirtd =  mkIf (cfg.virtualisation.enable) {
+        enable = true;
+        qemu = {
+          swtpm.enable = true;
+          ovmf.enable = true;
+          ovmf.packages = [ pkgs.OVMFFull.fd ];
+        };
+      };
+      spiceUSBRedirection.enable = true;
+    };
+    services.spice-vdagentd.enable = cfg.virtualisation.enable;
   };
 }
