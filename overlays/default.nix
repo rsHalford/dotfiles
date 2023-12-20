f73d9a3c3da0254b5ea477b473e768956e496871
Richard Halford <dev@rshalford.com>
2023-12-20 18:20:17 +0000

    feat(overlay): tmux overlay to sixel supported version

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,6 +16,16 @@
     scripts.overlay
     (self: super: {
       inherit nixvim;
+      tmux = super.tmux.overrideAttrs (old: {
+        configureFlags = old.configureFlags ++ ["--enable-sixel"];
+        src = super.fetchFromGitHub {
+          owner = "tmux";
+          repo = "tmux";
+          rev = "bdf8e614af34ba1eaa8243d3a818c8546cb21812";
+          hash = "sha256-ZMlpSOmZTykJPR/eqeJ1wr1sCvgj6UwfAXdpavy4hvQ=";
+        };
+        patches = [];
+      });
     })
     (final: prev: {
       godo = godo-flake.packages.${system}.godo;
