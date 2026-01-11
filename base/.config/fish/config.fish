d592682e8d6bebe6d927453f9e69823006018e2b
Richard Halford <dev@rshalford.com>
2026-01-11 17:18:41 +0000

    make use of ghostty built-ins rather than zellij

--- base/.config/fish/config.fish
+++ base/.config/fish/config.fish
@@ -3,7 +3,6 @@ set -g fish_greeting
 if status is-interactive
     set -x SSH_AUTH_SOCK "$HOME/.1password/agent.sock"
 
-    eval (zellij setup --generate-auto-start fish | string collect)
     zoxide init fish --cmd g | source
     mise activate fish | source
     starship init fish | source
